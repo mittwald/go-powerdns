6476ec99dcc53a91934f4379fe3542de313e734e
Mikhail Bruskov <mvbruskov@avito.ru>
2020-02-21 15:27:10 +0300

    fix codeclimate warnings

--- apis/cache/interface.go
+++ apis/cache/interface.go
@@ -5,5 +5,5 @@ import "context"
 // Client defines the interface for Cache operations.
 type Client interface {
 	// Flush flush a cache-entry by name
-	Flush(ctx context.Context, serverID string, name string) (*CacheFlushResult, error)
+	Flush(ctx context.Context, serverID string, name string) (*FlushResult, error)
 }
