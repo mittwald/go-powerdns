65a583c3640b208a15b32cb29dffd568f5394637
Jan-Philipp Benecke <jpb@cleverreach.com>
2020-05-30 09:06:41 +0200

    Move doc. line up

--- apis/cryptokeys/doc.go
+++ apis/cryptokeys/doc.go
@@ -1,6 +1,6 @@
+// Package cryptokeys contains a specialized client for interacting with PowerDNS' "Cryptokeys" API.
 //
 // More information
 //
 // Official API documentation: https://doc.powerdns.com/authoritative/http-api/cryptokey.html
-// Package cryptokeys contains a specialized client for interacting with PowerDNS' "Cryptokeys" API.
 package cryptokeys
