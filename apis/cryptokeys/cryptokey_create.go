25c2a15e5756aaf261f5354c38707af087ac8a03
Jan-Philipp Benecke <jpb@cleverreach.com>
2020-05-30 09:26:25 +0200

    Let add a Cryptokey for creating

--- apis/cryptokeys/cryptokey_create.go
+++ apis/cryptokeys/cryptokey_create.go
@@ -3,15 +3,16 @@ package cryptokeys
 import (
 	"context"
 	"fmt"
+	"github.com/mittwald/go-powerdns/pdnshttp"
 	"net/url"
 )
 
-func (c *client) CreateCryptokey(ctx context.Context, serverID, zoneID string) (*Cryptokey, error) {
+func (c *client) CreateCryptokey(ctx context.Context, serverID, zoneID string, opts Cryptokey) (*Cryptokey, error) {
 	cryptokey := Cryptokey{}
 	path := fmt.Sprintf("/api/v1/servers/%s/zones/%s/cryptokeys",
 		url.PathEscape(serverID), url.PathEscape(zoneID))
 
-	err := c.httpClient.Post(ctx, path, &cryptokey)
+	err := c.httpClient.Post(ctx, path, &cryptokey, pdnshttp.WithJSONRequestBody(opts))
 	if err != nil {
 		return nil, err
 	}
