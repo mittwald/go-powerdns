51929c63b1d01e94ec3f87e653a0210761a83c37
Michael Bruskov <mixanemca@yandex.ru>
2020-07-30 17:26:41 +0300

    Add human readable error message from PowerDNS API response

--- pdnshttp/errors.go
+++ pdnshttp/errors.go
@@ -13,10 +13,19 @@ func (e ErrNotFound) Error() string {
 type ErrUnexpectedStatus struct {
 	URL        string
 	StatusCode int
+	ErrResponse
 }
 
 func (e ErrUnexpectedStatus) Error() string {
-	return fmt.Sprintf("unexpected status code %d: %s", e.StatusCode, e.URL)
+	if len(e.ErrResponse.Messages) > 0 {
+		return fmt.Sprintf("unexpected status code %d: %s %s %s", e.StatusCode, e.URL, e.ErrResponse.Message, e.ErrResponse.Messages)
+	}
+	return fmt.Sprintf("unexpected status code %d: %s %s", e.StatusCode, e.URL, e.ErrResponse.Message)
+}
+
+type ErrResponse struct {
+	Message  string   `json:"error"`
+	Messages []string `json:"errors,omitempty"`
 }
 
 func IsNotFound(err error) bool {
